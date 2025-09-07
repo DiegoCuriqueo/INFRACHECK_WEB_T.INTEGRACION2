fb553f0464efd0c30aa5363c9714ac60b2b35031
ALEXIS MONSALVE H. <alexis.monsalve2023@alu.uct.cl>
2025-09-07 12:52:06 -0300

    Delete infracheck/frontend directory

--- infracheck/frontend/src/context/UserContext.jsx
+++
@@ -1,8 +0,0 @@
-import { createContext, useContext, useState } from "react";
-const UserCtx = createContext(null);
-export const useUser = () => useContext(UserCtx);
-
-export function UserProvider({ children }) {
-  const [user, setUser] = useState(null); // llena desde login o API
-  return <UserCtx.Provider value={{ user, setUser }}>{children}</UserCtx.Provider>;
-}
